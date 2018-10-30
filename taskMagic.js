782bfff774d36fbd9c3aff631d57d9680da49dac
Collin Argo <collinargo@gmail.com>
2018-10-30 18:23:51 -0400

    add actual Task objects

--- taskMagic.js
+++ taskMagic.js
@@ -1,16 +1,38 @@
 
 // elements from the page
 const body = document.querySelector('body')
+const parentTaskName = document.getElementById('parentTaskName')
 const taskSearch = document.getElementById('taskSearch')
 const tasksDiv = document.querySelector('div.tasks')
 const taskCellPrototype = tasksDiv.firstElementChild.cloneNode(true)
 const addTaskButton = document.getElementById('addTask')
+// will load parent task from cache later
+const parentTask = Task(parentTaskName.textContent)
 
-
+// event listeners
 addTaskButton.addEventListener('click', addTask)
+
+// objects
+function Task(taskName) {
+    return {
+        name: taskName,
+        parent: [],
+        children: [],
+        addTask(task) {
+            this.children.push(task)
+            task.parents.push(this)
+        },
+    }
+}
+
+//functions
 
 // get a task clone
 function addTask() {
+    // don't add an unnamed task
+    if (getSearchTextValue() == '') {
+        return
+    }
     // use prototype in case first task is not good clone choice
     let newTask = taskCellPrototype.cloneNode(true)
     let newTaskName = newTask.firstElementChild
@@ -20,10 +42,14 @@ function addTask() {
     // save task to file here
 
     // update task appearance
-    newTaskName.textContent = taskSearch.value
+    newTaskName.textContent = getSearchTextValue()
     newTaskChildrensNames.textContent = ''
     tasksDiv.appendChild(newTask)
     taskSearch.value = ''
+}
+
+function getSearchTextValue() {
+    return taskSearch.value
 }
 
 

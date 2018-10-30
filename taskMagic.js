01573c33e6396ba647f1f6e7f2f4208117253e36
Collin Argo <collinargo@gmail.com>
2018-10-30 14:57:55 -0400

    create wireframe

---
+++ taskMagic.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body')
+const tasksDiv = document.querySelector('div.tasks')
+// clone tasks for example
+for (let i = 0; i < 20; i++) {
+    let newTask = document.getElementsByClassName('task')[0].cloneNode(true)
+    tasksDiv.appendChild(newTask)
+}

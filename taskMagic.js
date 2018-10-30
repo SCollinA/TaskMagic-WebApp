c36594872e4913108184a060586a8d1e4adad7b3
Collin Argo <collinargo@gmail.com>
2018-10-30 16:55:32 -0400

    move searchbar and toolbar into div with tasks

--- taskMagic.js
+++ taskMagic.js
@@ -3,8 +3,8 @@ const tasksDiv = document.querySelector('div.tasks')
 
 
 
-// // clone tasks for example
-// for (let i = 0; i < 20; i++) {
-//     let newTask = document.getElementsByClassName('task')[0].cloneNode(true)
-//     tasksDiv.appendChild(newTask)
-// }
+// clone tasks for example
+for (let i = 0; i < 20; i++) {
+    let newTask = document.getElementsByClassName('task')[0].cloneNode(true)
+    tasksDiv.appendChild(newTask)
+}

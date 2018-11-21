b2d3b2fb953de0babbfffe772a07f5393cd57c0c
Collin Argo <collinargo@gmail.com>
2018-11-21 15:20:01 -0500

    fix user authentication/route protections

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Task = require('./models/Task')
 const taskViewTemplate = require('./views/taskView.js')
 const taskView = taskViewTemplate.taskView
 const headerView = taskViewTemplate.header
-const createTaskCells = taskViewTemplate.taskCells
+const taskCells = taskViewTemplate.taskCells
 const loginView = require('./views/loginView')
 const registerView = require('./views/registerView')
 
@@ -50,7 +50,7 @@ let currentUser
 let currentTask
 let previousTasks = []
 
-app.get('/', (req, res) => res.redirect('/login'))
+// app.get('/', (req, res) => res.redirect('/login'))
 
 app.get('/login', (req, res) => {
     res.send(loginView())
@@ -64,7 +64,7 @@ app.post('/login', (req, res) => {
     .then(user => {
         if (user.matchPassword(password)) {
             req.session.user = user
-            res.redirect(`/user/${user.id}`)
+            res.redirect(`/`)
         } else {
             res.redirect('/login');
         }
@@ -91,40 +91,28 @@ app.post('/register', (req, res) => {
         Task.add(`${userName}'s life`)
         .then(task => {
             task.assignToUser(user.id)
-            .then(() => res.redirect(`/user/${user.id}`))
+            .then(() => res.redirect(`/`))
         })
     })
 })
 // define endpoints
 // listen for get requests
-app.get('/user/:userID([0-9]+)', (req, res) => {
-    User.getById(req.params.userID)
+app.get('/', protectRoute, (req, res) => {
+    User.getById(req.session.user.id)
     // .then(console.log)
     .then(user => {
         currentUser = user
         user.rootTask().then(task => {
             currentTask = task
-            res.redirect(`/task/${task.id}`)
-            // const header = headerView(task, previousTasks[0])
-            // res.send(taskView(header, children))
-        })
-    })
-})
-
-
-// doing the task managing sutff
-app.get("/task/:taskID([0-9]+)", (req, res) => {
-    Task.getById(req.params.taskID)
-    .then(task => {
-        task.getUsers()
-        .then(users => {
-            // if this task has current logged in user assigned to it
-            if (users.map(user => user.id).includes(req.session.user.id)) {
+            task.getUsers()
+            .then(() => {
+                // if this task has current logged in user assigned to it
+                // if (users.map(user => user.id).includes(req.session.user.id)) {
                 task.getChildren()
                 .then(children => {
                     if (!currentTask) {
                         currentTask = task
-                    }
+                    }   
                     // if we are not at a previous task already
                     if (currentTask.id != task.id && !previousTasks.map(prevTask => prevTask.id).includes(task.id)) {
                         previousTasks.unshift(currentTask)
@@ -136,33 +124,68 @@ app.get("/task/:taskID([0-9]+)", (req, res) => {
                     currentTask = task
                     console.log(currentTask)
                     const header = headerView(task, previousTasks[0])
-                    createTaskCells(children)
+                    taskCells(children)
                     .then(taskCells => res.send(taskView(header, taskCells)))
                 })
-            } else {
-                res.redirect('/login')
-            }
+                // } else {
+                //     res.redirect('/login')
+                // }
+            })
+            // res.redirect(`/task/${task.id}`)
+            // const header = headerView(task, previousTasks[0])
+            // res.send(taskView(header, children))
         })
     })
 })
 
-app.post("/task/:parentTaskID([0-9]+)", (req, res) => {
+
+// doing the task managing sutff
+// app.get("/task/:taskID([0-9]+)", (req, res) => {
+//     Task.getById(req.params.taskID)
+//     .then(task => {
+//         task.getUsers()
+//         .then(users => {
+//             // if this task has current logged in user assigned to it
+//             if (users.map(user => user.id).includes(req.session.user.id)) {
+//                 task.getChildren()
+//                 .then(children => {
+//                     if (!currentTask) {
+//                         currentTask = task
+//                     }
+//                     // if we are not at a previous task already
+//                     if (currentTask.id != task.id && !previousTasks.map(prevTask => prevTask.id).includes(task.id)) {
+//                         previousTasks.unshift(currentTask)
+//                         console.log(previousTasks)
+//                     } else if (previousTasks.length > 0 && task.id == previousTasks[0].id) {
+//                         previousTasks.shift()
+//                         console.log(previousTasks)
+//                     }
+//                     currentTask = task
+//                     console.log(currentTask)
+//                     const header = headerView(task, previousTasks[0])
+//                     createTaskCells(children)
+//                     .then(taskCells => res.send(taskView(header, taskCells)))
+//                 })
+//             } else {
+//                 res.redirect('/login')
+//             }
+//         })
+//     })
+// })
+
+app.post("/", protectRoute, (req, res) => {
     // console.log(req.body)
-    if (currentUser) { // if a user is logged in
-        Task.add(req.body.taskSearch)
-        .then(task => {
-            task.assignToUser(currentUser.id)
+    Task.add(req.body.taskSearch)
+    .then(task => {
+        task.assignToUser(currentUser.id)
+        .then(() => {
+            currentTask.addChild(task)
+            // task.addParent(currentTask)
             .then(() => {
-                currentTask.addChild(task)
-                // task.addParent(currentTask)
-                .then(() => {
-                    res.redirect(`/task/${currentTask.id}`)
-                })
+                res.redirect(`/`)
             })
         })
-    } else {
-        res.send('Not logged in...')
-    }
+    })
 })
 
 app.listen(port, () => console.log(`My Task App listening on port ${port}!`))

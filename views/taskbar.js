dbbe59b6a06a0f654eada3895acd9c6680adb331
Collin Argo <collinargo@gmail.com>
2018-12-03 10:46:27 -0500

    add share task button to toolbar

--- views/taskbar.js
+++ views/taskbar.js
@@ -1,6 +1,9 @@
 const taskbar = function() {
     return `
     <div class="toolbar">
+        <div id="shareTask">
+            <h4><-task</h4> 
+        </div>
         <div id="addTask">
             <h4>+task</h4> 
         </div>
